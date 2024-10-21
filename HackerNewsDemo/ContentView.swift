c553ce0dd501f7f8935aa652849ab56a76301910
alextuchkov <alextychkoff@gmail.com>
2024-10-21 12:00:54 +0300

    Initial Commit

---
+++ HackerNewsDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HackerNewsDemo
+//
+//  Created by Oleksandr Tuchkov on 21.10.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HackerNewsDemo/HackerNewsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsDemoApp.swift
+//  HackerNewsDemo
+//
+//  Created by Oleksandr Tuchkov on 21.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
